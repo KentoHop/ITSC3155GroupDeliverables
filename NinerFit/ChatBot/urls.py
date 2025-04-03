6433ef844629bc7dc3795cb69b42af7593daa94c
KentoHop <kentohopkins77@gmail.com>
2025-04-03 07:41:11 -0400

    Update urls.py

--- NinerFit/ChatBot/urls.py
+++ NinerFit/ChatBot/urls.py
@@ -3,7 +3,4 @@ from . import views
  
 urlpatterns = [
     path('chatbot', views.chatbot, name='chatbot'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
-    path('logout', views.logout, name='logout'),
 ]
