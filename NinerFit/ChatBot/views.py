035513c0b4980d8cf1cd5dbcbc3bfd46a39b96ea
KentoHop <kentohopkins77@gmail.com>
2025-04-03 04:46:07 -0400

    dev chatbot 3

--- NinerFit/ChatBot/views.py
+++ NinerFit/ChatBot/views.py
@@ -8,7 +8,7 @@ client = OpenAI(api_key=your_api)
 # Function to interact with OpenAI ChatGPT
 def ask_openai(message):
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",  # Use "gpt-4" if needed after development done
+        model="gpt-4",  # Use "gpt-4-turbo" if needed after development done
         messages=[{"role": "user", "content": message}],
         max_tokens=150,
         temperature=0.7
@@ -23,3 +23,12 @@ def chatbot(request):
         response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def logout(request):
+    return redirect('login')
