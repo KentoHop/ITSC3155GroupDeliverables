6c619e6ca7ddd5d47c0d796736d6faf042ea9407
Benjamin Nguyen <bnguye40@charlotte.edu>
2025-04-04 02:14:57 -0400

    Edited logout button

--- NinerFit/ChatBot/views.py
+++ NinerFit/ChatBot/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from openai import OpenAI
 from django.contrib import auth
+from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from .models import Chat
 from django.utils import timezone
@@ -15,7 +16,7 @@ client = OpenAI(api_key=your_api)
 # Function to interact with OpenAI ChatGPT
 def ask_openai(message):
     response = client.chat.completions.create(
-        model="gpt-4",  # Use "gpt-4-turbo" if needed after development
+        model="gpt-4o",  # Use "gpt-4-turbo" if needed after development
         messages=[
             {"role": "system", "content": "You are a counselor supporting users trying to improve their mental health. If a user expresses a serious crisis, provide emergency contact resources such as 911."},
             {"role": "user", "content": message},
@@ -37,3 +38,7 @@ def chatbot(request):
         chat.save()
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html', {'chats': chats})
+
+def logout_view(request):
+    logout(request)
+    return redirect('../')
