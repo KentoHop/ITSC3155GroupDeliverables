7fbb3e2fae71067cd879093badbd5263e8a071ce
monkpeh <monkpehk35@gmail.com>
2025-04-09 23:05:40 -0400

    finished Todolist

--- NinerFit/HealthScore/urls.py
+++ NinerFit/HealthScore/urls.py
@@ -1,7 +1,3 @@
-path('health-score/', views.healthScore, name="health-score"),
-    path('to-do-list/', views.toDoList, name="to-do-list"),
-    path('suggestions/', views.suggestions, name="suggestions"),
-
 from django.urls import path
 from . import views
 from Authentication.views import login, register, logout
@@ -9,5 +5,8 @@ from Authentication.views import login, register, logout
 urlpatterns = [
     path('health-score/', views.healthScore, name="health-score"),
     path('to-do-list/', views.toDoList, name="to-do-list"),
+    path('addtodo/', views.addTodo, name='addtodo'),
+    path('updatetodo/<int:pk>/', views.updateTodo, name='updatetodo'),
+    path('deletetodo/<int:pk>/', views.deleteTodo, name='deletetodo'),
     path('suggestions/', views.suggestions, name="suggestions"),
 ]
